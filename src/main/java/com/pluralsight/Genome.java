7f7645545eb567df2bd68031d923d45077fa3af1
Gabe <Carrollgbusiness@gmail.com>
2026-05-22 09:20:06 -0500

    Create Genome & Credits Class

---
+++ src/main/java/com/pluralsight/Genome.java
@@ -0,0 +1,73 @@
+package com.pluralsight;
+
+public class Genome {
+    private String Primate;
+    private String Insectoid;
+    private String Reptilian;
+    private String Avian;
+    private String Aquatic;
+    private String apexPred;
+
+    //methods
+
+    //Constructor
+
+    public Genome(String apexPred, String aquatic, String avian, String reptilian, String insectoid, String primate) {
+        this.apexPred = apexPred;
+        Aquatic = aquatic;
+        Avian = avian;
+        Reptilian = reptilian;
+        Insectoid = insectoid;
+        Primate = primate;
+    }
+
+    //Getter/Setter
+
+    public String getPrimate() {
+        return Primate;
+    }
+
+    public void setPrimate(String primate) {
+        Primate = primate;
+    }
+
+    public String getInsectoid() {
+        return Insectoid;
+    }
+
+    public void setInsectoid(String insectoid) {
+        Insectoid = insectoid;
+    }
+
+    public String getReptilian() {
+        return Reptilian;
+    }
+
+    public void setReptilian(String reptilian) {
+        Reptilian = reptilian;
+    }
+
+    public String getAvian() {
+        return Avian;
+    }
+
+    public void setAvian(String avian) {
+        Avian = avian;
+    }
+
+    public String getAquatic() {
+        return Aquatic;
+    }
+
+    public void setAquatic(String aquatic) {
+        Aquatic = aquatic;
+    }
+
+    public String getApexPred() {
+        return apexPred;
+    }
+
+    public void setApexPred(String apexPred) {
+        this.apexPred = apexPred;
+    }
+}
