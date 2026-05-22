04f0f79f5edfd37fa75219ef50511bb7edf99fb3
Gabe <Carrollgbusiness@gmail.com>
2026-05-22 13:31:19 -0500

    add home menu

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,11 +1,40 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class App {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-
-
-
-
-
+        homeMenu();
     }
+    public static void homeMenu(){
+        while (true){
+            System.out.println("================");
+            System.out.println("HELIX BIOWORKS");
+            System.out.println("================");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.println("================");
+            int homeInput = input.nextInt();
+
+
+
+                //calls methods with code for each function
+                switch (homeInput) {
+                    case 1:
+                        // open sub menu
+                        break;
+                    case 0:
+                        System.out.println("The program exits");
+                        return;
+                }
+
+            }
+
+        }
+        //menu methods
+
+
 }
+
+
