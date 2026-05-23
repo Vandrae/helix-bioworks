c4133d557bc28f81c4bea15383a8f858862e26d3
Gabe <Carrollgbusiness@gmail.com>
2026-05-22 22:03:15 -0500

    change classes to enum and fill out w/ methods

--- src/main/java/com/pluralsight/creature/Scale.java
+++ src/main/java/com/pluralsight/creature/Scale.java
@@ -1,61 +1,25 @@
 package com.pluralsight.creature;
 
-public class Scale {
-    private String Class1;
-    private String Class2;
-    private String Class3;
-    private String Class4;
-    private String Class5;
+import com.pluralsight.Credits;
 
-    //methods
+public enum Scale {
+    CLASS_I,
+    CLASS_II,
+    CLASS_III;
 
-    //constructor
-    public Scale(String class1, String class2, String class3, String class4, String class5) {
-        Class1 = class1;
-        Class2 = class2;
-        Class3 = class3;
-        Class4 = class4;
-        Class5 = class5;
-    }
-
-    //getter/setter
-    public String getClass1() {
-        return Class1;
-    }
-
-    public void setClass1(String class1) {
-        Class1 = class1;
-    }
-
-    public String getClass2() {
-        return Class2;
-    }
-
-    public void setClass2(String class2) {
-        Class2 = class2;
-    }
-
-    public String getClass3() {
-        return Class3;
-    }
-
-    public void setClass3(String class3) {
-        Class3 = class3;
-    }
-
-    public String getClass4() {
-        return Class4;
-    }
-
-    public void setClass4(String class4) {
-        Class4 = class4;
-    }
-
-    public String getClass5() {
-        return Class5;
-    }
-
-    public void setClass5(String class5) {
-        Class5 = class5;
+    public Credits getBasePrice(){
+       switch (this){
+           case CLASS_I -> {
+               return new Credits(800.00);
+           }
+           case CLASS_II -> {
+               return new Credits(12000.00);
+           }
+           case CLASS_III -> {
+               return new Credits(100000.00);
+           }
+           default -> throw new IllegalStateException("Unknown Scale Class");
+       }
     }
 }
+
