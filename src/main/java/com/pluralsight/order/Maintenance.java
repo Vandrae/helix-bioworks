75af67276584a4326555911b9c1ea2ab90dc1e58
Gabe <Carrollgbusiness@gmail.com>
2026-05-24 03:26:01 -0500

    Add Maintenance class to set price

--- src/main/java/com/pluralsight/order/Maintenance.java
+++ src/main/java/com/pluralsight/order/Maintenance.java
@@ -3,11 +3,9 @@ package com.pluralsight.order;
 import com.pluralsight.Credits;
 
 public class Maintenance {
+
     public Credits getPrice(){
+         return new Credits(150.00);
+
     }
-
-
-
-
-
 }
